0739e473c3abf1403ace151e63ea36a0ccf8c2ef
zep1994 <snaxbags@protonmail.com>
2018-12-12 13:46:19 -0600

    set Initial Timer

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Timer from './components/TImer'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Timer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Timer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Timer extends React.Component {
+    constructor(props) {
+    super(props)
+     this.state = {count: 0}
+    }
+    render() {
+        const {count} = this.state
+        return (
+            <div> 
+                <h1>Current Count: { count }</h1>
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        this.myInterval = setInterval(() => {
+            this.setState(prevState => ({
+                count: prevState.count + 1
+            }))
+        }, 1000)  
+    }
+}
+
+export default Timer;
