6dd338cac9362ddbab5575e9d537b4ddc645d1e3
zep1994 <snaxbags@protonmail.com>
2018-12-12 13:48:55 -0600

    lets go the other direction

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Timer from './components/TImer'
+import Timer from './components/Timer'
 import './App.css';
 
 class App extends Component {
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Timer extends React.Component {
     constructor(props) {
     super(props)
-     this.state = {count: 0}
+     this.state = {count: 200}
     }
     render() {
         const {count} = this.state
@@ -17,9 +17,13 @@ class Timer extends React.Component {
     componentDidMount() {
         this.myInterval = setInterval(() => {
             this.setState(prevState => ({
-                count: prevState.count + 1
+                count: prevState.count - 1
             }))
         }, 1000)  
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.myInterval)
     }
 }
 
